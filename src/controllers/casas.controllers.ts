e14c5f8666f40aa2d164dc585715f509a305785f
Matias Reyna <matiireyna@hotmail.com>
2024-04-24 16:50:18 -0300

    Controllers casas: in process

---
+++ src/controllers/casas.controllers.ts
@@ -0,0 +1,20 @@
+import casasInterface from '../types/casas.type';
+import Casas from '../models/casas.models';
+import { Request, Response } from 'express';
+
+export const createCasa = async (req: Request, res: Response) => {
+    try {
+        const casa: casasInterface = req.body;
+
+        const findCasa = await Casas.findOne({ nameModel: casa.nameModel });
+
+        if (findCasa) {
+            return res.status(400).json({ message: `La casa ${casa.nameModel} ya existe en la base de datos` });
+        }
+
+        const newCasa = await new Casas(casa).save();
+        return res.status(201).json(newCasa);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
