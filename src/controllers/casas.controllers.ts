a7fe1cd81e41e4cd152f689da89f3e70a02d45f4
Matias Reyna <matiireyna@hotmail.com>
2024-04-25 12:58:45 -0300

    Controller getCasaByName

--- src/controllers/casas.controllers.ts
+++ src/controllers/casas.controllers.ts
@@ -9,11 +9,27 @@ export const createCasa = async (req: Request, res: Response) => {
         const findCasa = await Casas.findOne({ nameModel: casa.nameModel });
 
         if (findCasa) {
-            return res.status(400).json({ message: `La casa ${casa.nameModel} ya existe en la base de datos` });
+            return res.status(404).json({ message: `La casa ${casa.nameModel} ya existe en la base de datos` })
         }
 
         const newCasa = await new Casas(casa).save();
         return res.status(201).json(newCasa);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const getCasaByName = async (req: Request, res: Response) => {
+    try {
+        const { nameModel } = req.query;
+
+        const findCasa = await Casas.findOne({ nameModel });
+
+        if (!findCasa) {
+            return res.status(404).json({ message: `No se encontró ninguna casa con el nombre '${nameModel}'`})
+        } else {
+            return res.status(200).json(findCasa)
+        }
     } catch (error) {
         return res.status(500).json({ message: error.message });
     }
