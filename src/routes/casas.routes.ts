23b57cd7d885d9cbebd62f806f670dc93d8f49c0
Matias Reyna <matiireyna@hotmail.com>
2024-04-25 16:11:18 -0300

    Probando cambio

--- src/routes/casas.routes.ts
+++ src/routes/casas.routes.ts
@@ -4,6 +4,6 @@ import { createCasa, getCasaByName } from '../controllers/casas.controllers';
 const router = Router();
 
 router.post('/', createCasa);
-router.get('/', getCasaByName);
+router.get('/', getCasaByName);  //Probando.
 
 export default router;
