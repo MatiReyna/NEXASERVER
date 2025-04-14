c5123dc1a300fc1df8ea66226a190bedbd5f94ba
Mati Reyna <matiireyna@hotmail.com>
2025-04-14 19:38:51 -0300

    Limpieza en en index de routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
-import promosRoutes from './promos.routes';
 import casasRoutes from './casas.routes';
 import userRoutes from './user.routes'
 import cloudinaryRoutes from './cloudinary.routes'
 
 const router = Router();
 
-router.use('/promos', promosRoutes);
 router.use('/casas', casasRoutes);
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/images', cloudinaryRoutes);
 
 export default router;
