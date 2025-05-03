84c283c841f462a9380b44cbe9c33da8ea5ac5f4
Mati Reyna <matiireyna@hotmail.com>
2025-05-02 23:02:50 -0300

    Agregamos validacion de variables en db

--- src/db.ts
+++ src/db.ts
@@ -3,9 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const requiredEnv = [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_PORT' ];
+requiredEnv.forEach((key) => {
+    if (!process.env[ key ]) {
+        throw new Error(`⚠️ Missing environment variable: ${ key }`);
+    }
+});
+
 export const sequelize = new Sequelize(
     process.env.DB_NAME as string,
-    process.env.DBR_USER as string,
+    process.env.DB_USER as string,
     process.env.DB_PASSWORD as string,
     {
         host: process.env.DB_HOST,
