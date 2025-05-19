d9f99311eb1e5e0ff1cfb04ec09054221c9cf1dc
Mati Reyna <matiireyna@hotmail.com>
2025-05-19 20:32:19 -0300

    CORS: consiguracion especifica - mejoras en manejo de error

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,27 @@ import routes from './routes/index.routes';
 const server = express();
 
 server.use(express.json());
-
 server.use(express.urlencoded({ extended: true }));
 
-server.use(cors());
+const allowedOrigins = [ 'http://localhost:5173', 'https://nexaclient.vercel.app' ]
+server.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
 
 server.use(morgan('dev'));
-
 server.use('/', routes);
+
+server.use((_req, res) => {
+    res.status(404).json({ message: 'Route not found' })
+});
 
 server.use((err: Error, _req:Request, res:Response, _next:NextFunction) => {
     console.error('❌ Error stack:', err.stack);
     return res.status(500).json({
         message: 'Something went wrong',
-        error: process.env.NODE_ENV === 'development' ? err.message : undefined
+        error: process.env.NODE_ENV === 'development' ? err.message : undefined,
+        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
     });
 });
 
