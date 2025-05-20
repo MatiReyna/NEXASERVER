c96626b564576c1d1b0c4092ce327c7299738d4b
Mati Reyna <matiireyna@hotmail.com>
2025-05-19 21:33:09 -0300

    Validacion de variables de entorno

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,5 +1,12 @@
 import { v2 as cloudinary } from 'cloudinary';
 
+const requiredCloudinaryEnv = [ 'CLOUD_NAME', 'API_KEY', 'API_SECRET' ];
+requiredCloudinaryEnv.forEach((key) => {
+    if (!process.env[ key ]) {
+        throw new Error(`⚠️ Missing environment variable: ${ key }`);
+    }
+});
+
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
