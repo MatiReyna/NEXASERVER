0018b00a646a33b00f3cc2ca29f8613b3505999d
Mati Reyna <matiireyna@hotmail.com>
2025-05-22 20:17:40 -0300

    Agregando campos nuevos

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -2,4 +2,8 @@ export interface UserAttributes {
     id?: string;
     email: string;
     password: string;
+    name?: string;
+    role: 'admin' | 'user';
+    createdAt?: Date;
+    updatedAt?: Date;
 }
